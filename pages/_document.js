bc0d6ada487051179bc806eb0106ada7b08ea7ac
Nahuel Santoalla <nahjersan@gmail.com>
2019-08-20 01:05:13 -0300

    fix material ui styles

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/styles'
+import { Colors } from '../model/Themes'
+
+export default class MyDocument extends Document {
+  static getInitialProps = async ctx => {
+    const sheets = new ServerStyleSheets()
+    const _renderPage = ctx.renderPage
+    ctx.renderPage = () => _renderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    })
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          {sheets.getStyleElement()}
+        </React.Fragment>,
+      ],
+    }
+  }
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content={Colors.primary.main} />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
