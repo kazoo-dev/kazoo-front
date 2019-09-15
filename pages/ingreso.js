924aa43be86e5d30ef2afdd99658f694b85e9063
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2019-09-15 11:50:08 -0300

    Corrección según comentarios de PR.

--- pages/ingreso.js
+++ pages/ingreso.js
@@ -1,14 +1,14 @@
-import { MyButton } from "../components/MyButton";
-import { Temas } from '../model/Temas'
-import { Grid, Link, Typography, TextField, CssBaseline } from '@material-ui/core';
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
-import { MensajeDeError } from "../components/MensajeDeError";
+import {MyButton} from "../components/MyButton";
+import {Temas} from '../model/Temas'
+import {CssBaseline, Grid, Link, TextField, Typography} from '@material-ui/core';
+import {Header} from "../components/Header";
+import {Footer} from "../components/Footer";
+import {MensajeDeError} from "../components/MensajeDeError";
 import Backend from "../model/Backend";
 import Router from 'next/router';
 import Layout from "../components/Layout";
-import { redirigirSiEstaAutenticado } from "../lib/Auth"
-import { setUsuario } from "../lib/Sesion"
+import {redirigirSiEstaAutenticado} from "../lib/Auth"
+import {setUsuario} from "../lib/Sesion"
 
 const { verde } = Temas;
 
@@ -26,7 +26,7 @@ class IngresoUsuario extends React.Component {
     };
 
     componentDidMount(props) {
-        redirigirSiEstaAutenticado();
+        redirigirSiEstaAutenticado("/");
     }
 
     actualizarUsuario(evento) {
