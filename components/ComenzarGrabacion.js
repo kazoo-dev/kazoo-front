f93b92c0c59d07ce6689f39fce82dbb58c2f5096
EXB29031 <ayelen.garcia@claro.com.ar>
2019-09-24 20:53:03 -0300

    [WIP] primera aproximación de dropzone

--- components/ComenzarGrabacion.js
+++ components/ComenzarGrabacion.js
@@ -1,18 +1,32 @@
-import { BPM } from './BPM';
+import {BPM} from './BPM';
 import Link from 'next/link';
-import { MyButton } from './MyButton';
+import {MyButton} from './MyButton';
+import DropzoneDialogCustom from "./Dropzone/DropzoneDialogCustom";
+import * as PropTypes from "prop-types";
+import {Component} from "react";
 
-export const ComenzarGrabacion = ({ pulso, reiniciarPulso }) => (
-  <div id="contenedor">
-    <h1>Ya podés empezar la grabación de tu melodía!</h1>
-    <BPM style={{ height: '1.5rem' }} pulso={pulso}/>
-    <div id="botonera">
-      <MyButton icon="timer_off" onClick={reiniciarPulso}>Reiniciar</MyButton>
-      <Link href={`/record?pulso=${pulso}`} as="/grabacion">
-        <MyButton icon='mic'>Grabar</MyButton>
-      </Link>
-    </div>
-    <style jsx>{`
+
+export class ComenzarGrabacion extends Component {
+
+  state = {file: []};
+
+  handleSave = (file) => this.setState({file: file});
+
+  render() {
+    const {pulso, reiniciarPulso} = this.props;
+    return (
+      <div id="contenedor">
+        <h1>Ya podés empezar la grabación de tu melodía!</h1>
+        <BPM style={{height: '1.5rem'}} pulso={pulso}/>
+        <div id="botonera">
+          <MyButton icon="timer_off" onClick={reiniciarPulso}>Reiniciar</MyButton>
+          <Link href={`/record?pulso=${pulso}`} as="/grabacion">
+            <MyButton icon='mic'>Grabar</MyButton>
+          </Link>
+          <DropzoneDialogCustom onSave={this.handleSave}/>
+
+        </div>
+        <style jsx>{`
       #contenedor {
         display: flex;
         flex-direction: column;
@@ -32,6 +46,13 @@ export const ComenzarGrabacion = ({ pulso, reiniciarPulso }) => (
         text-align: center;
       }
     `}
-    </style>
-  </div>
-);
+        </style>
+      </div>
+    );
+  }
+}
+
+ComenzarGrabacion.propTypes = {
+  pulso: PropTypes.any,
+  reiniciarPulso: PropTypes.any
+}
---
+++ components/Dropzone/DropzoneDialogCustom.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import {DropzoneDialog} from 'material-ui-dropzone'
+import {MyButton} from "../MyButton";
+
+export default class DropzoneDialogCustom extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClose() {
+    this.setState({
+      open: false
+    });
+  }
+
+  handleSave(file) {
+    this.setState({
+      open: false
+    });
+    this.props.onSave(file);
+  }
+
+  handleOpen() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MyButton icon="cloud_upload" onClick={this.handleOpen.bind(this)}>
+          subir audio
+        </MyButton>
+        <div id="#dropzone">
+        <DropzoneDialog
+          open={this.state.open}
+          onSave={this.handleSave.bind(this)}
+          acceptedFiles={['audio/*']}
+          filesLimit={1}
+          maxFileSize={5000000}
+          onClose={this.handleClose.bind(this)}
+          showFileNamesInPreview={true}
+          dropzoneText="Arrastrá tu audio acá o hacé click"
+          dialogTitle={""}
+        />
+        </div>
+        <style jsx>{`
+          #dropzone {
+            width: 50vw;
+          }
+        `}
+        </style>
+      </div>
+    );
+  }
+}
