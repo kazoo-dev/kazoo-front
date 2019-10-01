5f2a976325d44e19d68247f2d251f222dc9388f9
Rodrigo Ballestero <ballesterorj@gmail.com>
2019-10-01 19:11:39 -0300

    [WIP] Intento de pasar el archivo de audio hacia la vista de partitura.

--- components/ComenzarGrabacion.js
+++ components/ComenzarGrabacion.js
@@ -4,19 +4,26 @@ import {MyButton} from './MyButton';
 import DropzoneDialogCustom from "./Dropzone/DropzoneDialogCustom";
 import * as PropTypes from "prop-types";
 import {Component} from "react";
+import Router from 'next/router';
 
 
 export class ComenzarGrabacion extends Component {
 
   state = {file: []};
 
-  handleSave = (file) => this.setState({file: file});
+  handleSave = (file) => {
+    this.setState({file: file});
+    Router.push({
+      pathname: '/grabacion2',
+      query: {pulso: this.props.pulso}},
+    );
+  }
 
   render() {
     const {pulso, reiniciarPulso} = this.props;
     return (
       <div id="contenedor">
-        <h1>Ya podés empezar la grabación de tu melodía!</h1>
+        <h1>Ya podés empezar la grabación de tu m"elodía!</h1>
         <BPM style={{height: '1.5rem'}} pulso={pulso}/>
         <div id="botonera">
           <MyButton icon="timer_off" onClick={reiniciarPulso}>Reiniciar</MyButton>
