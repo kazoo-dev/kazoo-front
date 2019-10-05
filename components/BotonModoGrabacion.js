b0636d29b0acb23a93f4c2992aeb1e903d542e7e
Nahuel Santoalla <nahjersan@gmail.com>
2019-10-05 17:25:01 -0300

    add volver del modo edicion

--- components/BotonModoGrabacion.js
+++ components/BotonModoGrabacion.js
@@ -1,14 +1,14 @@
-import { BotonKazoo } from './BotonKazoo';
-import { AccionBotonKazoo } from './AccionBotonKazoo';
-import Icon from '@material-ui/core/Icon'
+import Icon from '@material-ui/core/Icon';
 import Router from 'next/router';
+import { AccionBotonKazoo } from './AccionBotonKazoo';
+import { BotonKazoo } from './BotonKazoo';
 
 
 export const BotonModoGrabacion = ({ grabacionTerminada, terminarGrabacion, pasarAModoEdicion, abrirModal }) => (
-    <BotonKazoo icono={grabacionTerminada ? 'apps' : 'stop' } 
-                onClick={ grabacionTerminada ? null : terminarGrabacion}>
-        <AccionBotonKazoo onClick={() => Router.push('/')}><Icon>delete</Icon></AccionBotonKazoo>
-        <AccionBotonKazoo onClick={abrirModal}><Icon>save_alt</Icon></AccionBotonKazoo>
-        <AccionBotonKazoo onClick={pasarAModoEdicion}><Icon>edit</Icon></AccionBotonKazoo>
-    </BotonKazoo>
+  <BotonKazoo icono={grabacionTerminada ? 'apps' : 'stop'}
+    onClick={grabacionTerminada ? null : terminarGrabacion}>
+    <AccionBotonKazoo onClick={() => Router.push('/')}><Icon>delete</Icon></AccionBotonKazoo>
+    <AccionBotonKazoo onClick={abrirModal}><Icon>save_alt</Icon></AccionBotonKazoo>
+    <AccionBotonKazoo onClick={pasarAModoEdicion}><Icon>edit</Icon></AccionBotonKazoo>
+  </BotonKazoo>
 )
