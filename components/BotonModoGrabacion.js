9eeb2ef02788ca813ee81cd2f1a03eabf44f1b44
EXB29031 <ayelen.garcia@claro.com.ar>
2019-10-05 17:51:28 -0300

    Botón de menú y loading cuando se sube un audio y no se graba

--- components/BotonModoGrabacion.js
+++ components/BotonModoGrabacion.js
@@ -1,14 +1,15 @@
-import { BotonKazoo } from './BotonKazoo';
-import { AccionBotonKazoo } from './AccionBotonKazoo';
+import {BotonKazoo} from './BotonKazoo';
+import {AccionBotonKazoo} from './AccionBotonKazoo';
 import Icon from '@material-ui/core/Icon'
 import Router from 'next/router';
 
 
-export const BotonModoGrabacion = ({ grabacionTerminada, terminarGrabacion, pasarAModoEdicion, abrirModal }) => (
-    <BotonKazoo icono={grabacionTerminada ? 'apps' : 'stop' } 
-                onClick={ grabacionTerminada ? null : terminarGrabacion}>
-        <AccionBotonKazoo onClick={() => Router.push('/')}><Icon>delete</Icon></AccionBotonKazoo>
-        <AccionBotonKazoo onClick={abrirModal}><Icon>save_alt</Icon></AccionBotonKazoo>
-        <AccionBotonKazoo onClick={pasarAModoEdicion}><Icon>edit</Icon></AccionBotonKazoo>
+export const BotonModoGrabacion = ({ grabacionTerminada, terminarGrabacion, pasarAModoEdicion, abrirModal, loading }) => (
+    <BotonKazoo icono={grabacionTerminada ? 'apps' : 'stop' }
+                  onClick={ grabacionTerminada ? null : terminarGrabacion}
+                  loading={loading}>
+          <AccionBotonKazoo onClick={() => Router.push('/')}><Icon>delete</Icon></AccionBotonKazoo>
+          <AccionBotonKazoo onClick={abrirModal}><Icon>save_alt</Icon></AccionBotonKazoo>
+          <AccionBotonKazoo onClick={pasarAModoEdicion}><Icon>edit</Icon></AccionBotonKazoo>
     </BotonKazoo>
 )
--- components/ComenzarGrabacion.js
+++ components/ComenzarGrabacion.js
@@ -1,18 +1,17 @@
 import * as PropTypes from "prop-types";
-import {Component} from "react";
 
 import DropzoneDialogCustom from "./Dropzone/DropzoneDialogCustom";
 import {BPM} from './BPM';
 import {MyButton} from './MyButton';
-import { LayoutPreGrabacion } from './LayoutPreGrabacion'
-import { MarcadorInicioDePulso } from './MarcadorInicioDePulso'
-import { Grabacion } from './Grabacion'
+import {LayoutPreGrabacion} from './LayoutPreGrabacion'
+import {MarcadorInicioDePulso} from './MarcadorInicioDePulso'
+import {Grabacion} from './Grabacion'
 
 export function ComenzarGrabacion({ pulso, onSiguienteEstado }) {
   return (
     <LayoutPreGrabacion>
       <div id="contenedor">
-        <h1>Ya podés empezar la grabación de tu m"elodía!</h1>
+        <h1>Ya podés empezar la grabación de tu melodía!</h1>
         <BPM style={{height: '1.5rem'}} pulso={pulso}/>
         <div id="botonera">
           <MyButton icon="timer_off" onClick={
