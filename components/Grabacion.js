e6eef7358ad578c9e45f632d2c09d687a093256e
Lautaro <garcia.a.lautaro@gmail.com>
2019-10-07 23:28:13 -0300

    Integrar la funcionalidad de modificar el metro
    Se puede modificar el metro antes de grabar el audio. Esa página muestra
    el metro seleccionado y luego, tanto la partitura como la sumatoria de
    las duraciones de las notas.

--- components/Grabacion.js
+++ components/Grabacion.js
@@ -14,7 +14,6 @@ const Partitura = dynamic(() => import('./Partitura'), { ssr: false });
 export class Grabacion extends Component {
   state = {
     compases: [],
-    metro: { numerador: 4, denominador: 4 },
     tonalidad: 'C',
     modoEdicion: false,
     edicionTonalidad: false,
@@ -26,16 +25,16 @@ export class Grabacion extends Component {
   componentDidMount() {
     if (this.props.file) {
       this.setState({grabacionTerminada:true});
-      detectarArchivo(this.props.file, this.props.pulso)
+      detectarArchivo(this.props.file, this.props.pulso, this.props.metro)
         .then(compases => this.cargarPagina(compases))
     } else {
-      this.setState({loading:false}),
+      this.setState({loading:false});
       Grabador.iniciarGrabacion(4 * this.props.pulso, this.procesarCompas);
     }
   }
 
   cargarPagina(compases){
-    this.setState({loading:false}),
+    this.setState({loading:false});
     this.setState({compases});
   }
 
@@ -44,7 +43,7 @@ export class Grabacion extends Component {
   }
 
   procesarCompas = (unFragmentoDeAudio) => {
-    detectarFragmento(unFragmentoDeAudio).then(this.agregarCompas);
+    detectarFragmento(unFragmentoDeAudio, this.props.metro).then(this.agregarCompas);
   }
 
   terminarGrabacion = () => {
@@ -81,8 +80,8 @@ export class Grabacion extends Component {
   }
 
   guardarPartitura = (nombre) => {
-    const { compases, tonalidad, metro } = this.state;
-    const { numerador, denominador } = metro;
+    const { compases, tonalidad } = this.state;
+    const { numerador, denominador } = this.props.metro;
     Backend.guardarPartitura({ compases, tonalidad, numerador, denominador, nombre })
       .finally(() => Router.push('/partituras'));
   }
@@ -92,7 +91,7 @@ export class Grabacion extends Component {
       <Fragment>
         <Partitura scrollea={true}
           tonalidad={this.state.tonalidad}
-          metro={this.state.metro}
+          metro={this.props.metro}
           compases={this.state.compases} />
         {this.state.modoEdicion
           ? <BotonModoEdicion abrirSelectorTonalidad={this.abrirSelectorTonalidad}
---
+++ components/SelectorMetro.js
@@ -0,0 +1,93 @@
+import Icon from '@material-ui/core/Icon';
+import { Component } from 'react';
+
+export class SelectorMetro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { numerador: props.metro.numerador, denominador: props.metro.denominador };
+  }
+
+  aumentarDenominador() {
+    this.setState({ denominador: this.state.denominador + 1 });
+  }
+
+  disminuirDenominador() {
+    this.setState({ denominador: Math.max(1, this.state.denominador - 1) });
+  }
+
+  aumentarNumerador() {
+    this.setState({ numerador: Math.min(this.state.denominador, this.state.numerador + 1) });
+  }
+
+  disminuirNumerador() {
+    this.setState({ numerador: Math.max(1, this.state.numerador - 1) });
+  }
+
+  render() {
+    return <div id="selector-metro">
+      <span className="titulo">Modificá el metro</span>
+      <span className="up" onClick={this.aumentarNumerador.bind(this)}><Icon>keyboard_arrow_up</Icon></span>
+      <span className="numero">{this.state.numerador}</span>
+      <span className="down" onClick={this.disminuirNumerador.bind(this)}><Icon>keyboard_arrow_down</Icon></span>
+      <span id="espaciador"/>
+      <span className="up" onClick={this.aumentarDenominador.bind(this)}><Icon>keyboard_arrow_up</Icon></span>
+      <span className="numero">{this.state.denominador}</span>
+      <span className="down" onClick={this.disminuirDenominador.bind(this)}><Icon>keyboard_arrow_down</Icon></span>
+      <div id="botonera">
+        <Icon fontSize="large" onClick={() => this.props.alSeleccionar({
+          numerador: this.state.numerador,
+          denominador: this.state.denominador
+        })}>done</Icon>
+        <Icon fontSize="large" onClick={this.props.alCancelar}>clear</Icon>
+      </div>
+      <style jsx>{`
+      #selector-metro {
+        color: white;
+        background-color: rgb(0, 0, 0, 0.8);
+        height: 100vh;
+        width: 100vw;
+        z-index: 10;
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+      
+      .numero {
+        font-size: 8rem;
+      }
+      
+      .titulo {
+        font-size: 2rem;
+        margin-bottom: 5vh;
+      }
+      
+      #espaciador {
+        width: 25%;
+        border-bottom: solid white 1px;
+        margin-bottom: 2.5vh;
+        margin-top: 2vh;
+      }
+      
+      .up {
+        margin-bottom: -25px;
+        z-index: 11;
+      }
+      
+      .down {
+        margin-top: -25px;
+        z-index: 11;
+      }
+      
+      #botonera {
+        height: 7vw;
+        display: flex;
+        justify-content: space-between;
+        width: 50%;
+        margin-top: 5vh;
+      }
+    `}</style>
+    </div>;
+  }
+}
