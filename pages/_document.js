9f9a76230c4a084010ad4515f1bab0df20ed009f
Achus619 <achus619@gmail.com>
2019-11-11 22:35:09 -0300

    Agrego title a _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
           <link rel="shortcut icon" type="image/x-icon" href="static/img/favicon.ico" />
+          <title>Kazoo</title>
           <style>{`
             html, body, #__next {
               height: 100%;
